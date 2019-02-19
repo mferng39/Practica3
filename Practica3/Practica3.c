411d7471aad677028ad293ff0b5d50d1a9bdc510
mferng39 <mferng39@estudiantes.unileon.es>
2019-02-19 12:41:35 +0100

    first commit

---
+++ Practica3/Practica3.c
@@ -0,0 +1,37 @@
+
+#include <stdio.h>
+int main ()
+{
+	int Num, mes;
+	printf("Escribe un número:.. ");
+	scanf(" %d", &Num);
+	switch (Num){
+
+		case 1: printf("El mes es Enero\n");
+		break;
+		case 2: printf("El mes es Febrero\n");
+		break;		
+		case 3: printf("El mes es Marzo\n");	
+		break;	
+		case 4: printf("El mes es Abril\n");	
+		break;	
+		case 5: printf("El mes es Mayo\n");	
+		break;	
+		case 6: printf("El mes es Junio\n");
+		break;	
+		case 7: printf("El mes es Julio\n");	
+		break;	
+		case 8:printf("El mes es Agosto\n");	
+		break;	
+		case 9: printf("El mes es Septiembre\n");
+		break;	
+		case 10: printf("El mes es Octubre\n");	
+		break;	
+		case 11: printf("El mes es Noviembre\n");
+		break;	
+		case 12: printf("El mes es Dicciembre\n");
+		break;	
+}
+	return 0;
+}
+
